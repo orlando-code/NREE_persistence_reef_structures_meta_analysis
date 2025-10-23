b905113cbc213a750c26fff9a5caa7454c96d236
Orlando Timmerman <rt582@cam.ac.uk>
2025-10-23 19:52:08 +0100

    updated directory paths

--- calcification_meta_analysis/utils/config.py
+++ calcification_meta_analysis/utils/config.py
@@ -15,13 +15,10 @@ def get_repo_root() -> Path:
 
 # REPO DIRECTORIES
 repo_dir = get_repo_root()
+module_dir = repo_dir / "calcification_meta_analysis"
 resources_dir = repo_dir / "resources"
-meta_2022_dir = repo_dir / "meta_2022"
-module_dir = repo_dir / "calcification"
 fig_dir = repo_dir / "figures"
 
 # DATA DIRECTORIES
 data_dir = repo_dir / "data"
-clean_data_dir = data_dir / "clean"
 climatology_data_dir = data_dir / "climatology"
-tmp_data_dir = data_dir / "tmp"
