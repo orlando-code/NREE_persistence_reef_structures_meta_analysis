a7980a0775ad3475631d51267c75f917d7b826f6
Orlando Timmerman <rt582@cam.ac.uk>
2025-11-17 10:06:07 +0000

    removed old cbsyst investigation, improved documentation of remaining functions

--- calcification_meta_analysis/utils/utils.py
+++ calcification_meta_analysis/utils/utils.py
@@ -6,103 +6,16 @@ import numpy as np
 import pandas as pd
 from PIL import Image
 
-# ### cbsyst sensitivity analysis
-# def create_st_ft_sensitivity_array(
-#     param_combinations: list,
-#     pertubation_percentage: float,
-#     resolution: int = 20,
-#     progress_bar: bool = True,
-# ) -> xa.DataArray:
-#     # check if more than three parameters are passed
-#     if len(param_combinations[0]) != 3:
-#         raise ValueError(
-#             "param_combinations should be a list of tuples containing salinity, temperature, and pH_NBS values"
-#         )
 
-#     results_dict = {}
-#     for Sal, Temp, pH_NBS in tqdm(param_combinations, disable=not progress_bar):
-#         ST_base = cbh.calc_ST(Sal)
-#         FT_base = cbh.calc_FT(Sal)
+def convert_png_to_jpg(directory: str | Path):
+    """Convert all PNG files in a directory to JPG format.
 
-#         # define perturbation ranges for ST and FT
-#         ST_values = np.linspace(
-#             ST_base - pertubation_percentage / 100 * ST_base,
-#             ST_base + pertubation_percentage / 100 * ST_base,
-#             resolution,
-#         )
-#         FT_values = np.linspace(
-#             FT_base - pertubation_percentage / 100 * FT_base,
-#             FT_base + pertubation_percentage / 100 * FT_base,
-#             resolution,
-#         )
+    Args:
+        directory (str | Path): The directory to convert PNG files to JPG format.
 
-#         # initialize empty array for pH_total values
-#         pH_grid = np.zeros((resolution, resolution))
-
-#         for i, ST in enumerate(ST_values):
-#             for j, FT in enumerate(FT_values):
-#                 pH_Total = cbh.pH_scale_converter(
-#                     pH=pH_NBS, scale="NBS", ST=ST, FT=FT, Temp=Temp, Sal=Sal
-#                 ).get("pHtot", None)
-#                 pH_grid[i, j] = pH_Total
-#         results_dict[(Sal, Temp, pH_NBS)] = pH_grid.copy()
-
-#     # extract unique values for each dimension
-#     Sal_values = sorted(set(k[0] for k in results_dict.keys()))
-#     Temp_values = sorted(set(k[1] for k in results_dict.keys()))
-#     pH_NBS_values = sorted(set(k[2] for k in results_dict.keys()))
-#     # store arrays with dimensions metadata
-#     data_array = np.empty(
-#         (
-#             len(Sal_values),
-#             len(Temp_values),
-#             len(pH_NBS_values),
-#             len(ST_values),
-#             len(FT_values),
-#         )
-#     )
-#     for (v1, v2, v3), arr in results_dict.items():
-#         i = Sal_values.index(v1)
-#         j = Temp_values.index(v2)
-#         k = pH_NBS_values.index(v3)
-#         data_array[i, j, k, :, :] = arr
-
-#     return xa.DataArray(
-#         data_array,
-#         dims=["salinity", "temperature", "ph_nbs", "ST", "FT"],
-#         coords={
-#             "salinity": Sal_values,
-#             "temperature": Temp_values,
-#             "ph_nbs": pH_NBS_values,
-#             "ST": ST_values,
-#             "FT": FT_values,
-#         },
-#         name="pH_Total",
-#     )
-
-
-# def select_by_stat(ds: xa.Dataset, variables_stats: dict) -> xa.Dataset:
-#     """
-#     Selects values from an xarray dataset based on the specified statistics for given variables.
-
-#     Parameters:
-#         ds (xr.Dataset): The input dataset with a 'param_combination' dimension and coordinates for the variables.
-#         variables_stats (dict): A dictionary where keys are variable names and values are the statistics to use for selection ('min', 'max', 'mean').
-
-#     Returns:
-#         xr.Dataset: Dataset subset at the selected coordinate values.
-#     """
-#     allowed = {"min", "max", "mean"}
-#     selected_coords = {}
-#     for var, stat in variables_stats.items():
-#         if stat not in allowed:
-#             raise ValueError(f"stat for {var} must be one of {allowed}")
-#         selected_coords[var] = getattr(ds[var], stat)().item()
-#     return ds.sel(selected_coords, method="nearest")
-
-
-def convert_png_to_jpg(directory: str):
-    """Convert all PNG files in a directory to JPG format."""
+    Returns:
+        generator: A generator of the converted JPG files.
+    """
     png_files = Path(directory).glob("*.png")
     for png_file in png_files:
         with Image.open(png_file) as img:
@@ -123,19 +36,9 @@ def uniquify_repeated_values(vals: list, uniquify_str: str = "LOC") -> list:
     Returns:
         list: List of values with unique suffixes.
     """
-
-    def zip_letters(letters: list[str]) -> list[str]:
-        """Zip a list of strings with uppercase letters."""
-        al = string.ascii_uppercase
-        return (
-            [f"-{uniquify_str}-".join(i) for i in zip(letters, al)]
-            if len(letters) > 1
-            else letters
-        )
-
     from collections import Counter, defaultdict
 
-    # Precompute counts for all values to avoid repeated .count() calls
+    # precompute counts for all values to avoid repeated .count() calls
     value_counts = Counter(vals)
     counts = defaultdict(int)
     result = []
@@ -151,7 +54,14 @@ def uniquify_repeated_values(vals: list, uniquify_str: str = "LOC") -> list:
 
 
 def safe_to_numeric(col):
-    """Convert column to numeric if possible, otherwise return as is."""
+    """Convert column to numeric if possible, otherwise return as is.
+
+    Args:
+        col (pd.Series): The column to convert to numeric.
+
+    Returns:
+        pd.Series: The converted column.
+    """
     try:
         return pd.to_numeric(col)
     except (ValueError, TypeError):
@@ -159,7 +69,15 @@ def safe_to_numeric(col):
 
 
 def aggregate_df(df: pd.DataFrame, method: str = "mean") -> pd.DataFrame:
-    """Aggregate DataFrame by specified method (mean, median, etc.)"""
+    """Aggregate DataFrame by specified method (mean, median, etc.).
+
+    Args:
+        df (pd.DataFrame): The dataframe to aggregate.
+        method (str): The method to use for aggregation (mean, median, etc.).
+
+    Returns:
+        pd.DataFrame: The aggregated dataframe.
+    """
     aggregation_funcs = {
         col: method if pd.api.types.is_numeric_dtype(df[col]) else lambda x: x.iloc[0]
         for col in df.columns
@@ -181,12 +99,27 @@ def calc_sd_from_se(se: float, n: int) -> float:
 
 
 def round_down_to_nearest(x: float, step: float) -> float:
-    """Round down to the nearest multiple of step."""
+    """Round down to the nearest multiple of step.
+
+    Args:
+        x (float): The value to round down.
+        step (float): The step size e.g. 0.5 for rounding to the nearest 0.5.
+    Returns:
+        float: The rounded down value.
+    """
     return step * np.floor(x / step)
 
 
 def get_unique_values(df: pd.DataFrame, column: str) -> list[str]:
-    """Get unique values from a column."""
+    """Get unique values from a column.
+
+    Args:
+        df (pd.DataFrame): The dataframe to get unique values from.
+        column (str): The column to get unique values from.
+
+    Returns:
+        list[str]: The unique values from the column. If no unique values are found, an empty list is returned.
+    """
     if column in df.columns:
         unique_vals = df[column].dropna().unique().tolist()
         return sorted([str(v) for v in unique_vals])
